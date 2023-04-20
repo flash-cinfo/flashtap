890bbb5716cb1eab88c0c3163e9ff1ba6671a93d
senhorstd <r3s1zetv@gmail.com>
2023-04-20 17:40:35 +0000

    add imgui window size

--- src/Source.cpp
+++ src/Source.cpp
@@ -9,15 +9,8 @@ using namespace std;
 
 int main() {
     Shooter game;
-
-    // Optimal for standard monitors:
-    //game.create(720, 480, ShooterConsts::PROJECT_NAME, true);
-    game.create(1280, 720, ShooterConsts::PROJECT_NAME, true);
-    //game.create(1920, 1080, ShooterConsts::PROJECT_NAME, true, Consts::BACKGROUND_COLOR, sf::Style::Fullscreen);
-
-    // Optimal for MacBook Pro 16 display:
-    //game.create(2048, 1152, ShooterConsts::PROJECT_NAME, true, Consts::BACKGROUND_COLOR);
-    //game.create(3072, 1920, ShooterConsts::PROJECT_NAME, true, Consts::BACKGROUND_COLOR, sf::Style::Fullscreen);
+    game.create(1280, 720, ShooterConsts::PROJECT_NAME, false);
+    //game.create(1280, 720, ShooterConsts::PROJECT_NAME, true, Consts::BACKGROUND_COLOR, sf::Style::Fullscreen);
 
     return 0;
 }
--- src/debug/MenuInit.cpp
+++ src/debug/MenuInit.cpp
@@ -1,37 +1,40 @@
+#include "imgui/imgui.h"
 #pragma warning(disable : 4834)
 #include "MenuInit.h"
 #include <SFML/Window/Keyboard.hpp>
 
 void DebugMenuInit::IsTabbed() {
-    if (keyboardClaz->isKeyTapped(sf::Keyboard::Tab)) {
-        if (isOpen == false) {
-            isOpen = true;
-        } else if (isOpen == true) {
-            isOpen = false;
-        }
+  if (keyboardClaz->isKeyTapped(sf::Keyboard::Tab)) {
+    if (isOpen == false) {
+      isOpen = true;
+    } else if (isOpen == true) {
+      isOpen = false;
     }
+  }
 }
 
 void DebugMenuInit::DebugMenui() {
-    ImGui::Begin("Debug Menu");
-    ImGui::End();
+  ImGui::SetNextWindowSize(ImVec2(400, 400));
+  ImGui::Begin("Debug Menu");
+  ImGui::End();
 }
-
 
 void DebugMenuInit::Init(const std::shared_ptr<sf::RenderWindow> _window) {
-    if (ImGui::SFML::Init(*_window, true)) {
-        Log::log("DebugMenuInit::Init() -> ImGui::SFML::Init(window, true) -> initilized succes");
-    }
+  if (ImGui::SFML::Init(*_window, true)) {
+    Log::log("DebugMenuInit::Init() -> ImGui::SFML::Init(window, true) -> "
+             "initilized succes");
+  }
 }
 
-void DebugMenuInit::Update(const std::shared_ptr<sf::RenderWindow> _window, sf::Clock *deltaClock) {
-    ImGui::SFML::Update(*_window, deltaClock->restart());
-    DebugMenuInit::IsTabbed();
-    if (isOpen) {
-        DebugMenuInit::DebugMenui();
-    }
+void DebugMenuInit::Update(const std::shared_ptr<sf::RenderWindow> _window,
+                           sf::Clock *deltaClock) {
+  ImGui::SFML::Update(*_window, deltaClock->restart());
+  DebugMenuInit::IsTabbed();
+  if (isOpen) {
+    DebugMenuInit::DebugMenui();
+  }
 }
 
 void DebugMenuInit::Render(const std::shared_ptr<sf::RenderWindow> _window) {
-    ImGui::SFML::Render(*_window);
+  ImGui::SFML::Render(*_window);
 }
